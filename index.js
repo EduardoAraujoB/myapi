59a98f5d521565a3da9dab490bfdf76b9008e309
unknown <ea0123450@gmail.com>
2019-05-18 13:14:19 -0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/my-api", { useNewUrlParser: true });
 
